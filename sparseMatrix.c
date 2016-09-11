4cffde177614d87103f0cff646456badf6f7cf86
Ishan Joshi <ishanjoshi02@gmail.com>
2016-09-11 13:20:22 +0530

    Update sparseMatrix.c

--- sparseMatrix.c
+++ sparseMatrix.c
@@ -1,92 +1,105 @@
-/*
- * Accept  conventional  matrix
- *  and  convert  it  into  sparse  matrix  using  structure
- * and perform  addition, simple and fast transpose
- */
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX 10
-int **mallocate(int **a,int row, int col) {
-	int k;
-	a = (int **)malloc(sizeof(int)*row);
-	for(k = 0;k<col;k++) {
-		a[k] = (int *)malloc(sizeof(int)*col);
+#define MAX 25
+void accept(int c[MAX][3],int row,int col) {
+	int i,j,k=1,var;
+	printf("Enter the Elements now\n");
+	for(i=0;i<row;i++) {
+		for(j=0;j<col;j++) {
+			scanf("%d",&var);
+			if(var!=0) {
+				c[k][0] = i;
+				c[k][1] = j;
+				c[k][2] = var;
+				k++;
+			}
+		}
 	}
-	return a;
+	printf("Done Accepting\n");
+	c[0][0] = row;
+	c[0][1] = col;
+	c[0][2] = k-1;
 }
-void display(int c[MAX][3],int row,int col) {
-	int i,k = 1,j;
-	for(i = 0;i < row;i++) {
-		for(j = 0;j < col;j++) {
-			if(c[k][0] == i && c[k][1] == j) {
+void display(int c[MAX][3]) {
+	int i,j,k=1;
+	for(i=0;i<c[0][0];i++) {
+		for(j=0;j<c[0][1];j++) {
+			if(c[k][0]==i && c[k][1]==j) {
 				printf("%d",c[k][2]);
 				k++;
 			}
 			else {
 				printf("%d",0);
 			}
+			printf("\t");
 		}
+		printf("\n");
 	}
 }
-void convert(int **a,int c[MAX][3],int row,int column) {
-	int i,j,p = 0,q = 0,r = 0;
-	for(i=0;i<row;i++) {
-		for(j=0;j<column;j++) {
-			if(*(*(a+i)+j)!=0) {
-				c[1][0] = i;
-				p++;
-				c[1][1] = j;
-				q++;
-				c[1][2] = **a;
-				r++;
+int max(int a,int b) {
+	if(a>b) {
+		return a;
+	}
+	return b;
+}
+void add(int c[MAX][3],int c1[MAX][3]){
+	int i,j,k=1,l=1,m=1;
+	int c2[MAX][3];
+	c2[0][0] = max(c[0][0],c1[0][0]);
+	c2[0][1] = max(c[0][1],c1[0][1]);
+	for(i=0;i<c2[0][0];i++) {
+		for(j=0;j<c2[0][1];j++) {
+			if(i==c[k][0] && j==c[k][1]) {
+				if(i==c1[l][0] && j==c1[l][1]) {
+					c2[m][0] = i;
+					c2[m][1] = j;
+					c2[m][2] = c[k][2] + c1[l][2];
+					l++;
+					continue;
+				}
+				else {
+					c2[m][0] = i;
+					c2[m][1] = j;
+					c2[m][2] = c[k][2];
+					continue;
+				}
+				k++;m++;
+			}
+			if(i==c1[l][0] && j==c1[l][1]) {
+				c2[m][0] = i;
+				c2[m][1] = j;
+				c2[m][2] = c1[l][2];
+				l++;m++;
 			}
 		}
 	}
-	c[0][0] = p;
-	c[0][1] = q;
-	c[0][2] = r;
+	display(c2);
 }
-void accept (int **a,int row,int column) {
-	int i,j;
-
-	printf("Enter the Elements now\n");
-	for(i=0;i<row;i++) {
-		for(j=0;j<column;j++) {
-			scanf("%d",(*(a+i)+j));
-		}
-	}
-}
-int main() {
-	int opt,row,row1,column,column1;
-	int **a,**b,c[MAX][3],c1[MAX][3];
+int main(int argc, char **argv) {
+	int opt,row,row1,col,col1;
+	int c[MAX][3],c1[MAX][3];
 	do {
-		printf("1.Accept a Sparse Matrix\n2.Display a Sparse Matrix\n3.Perform Addition of Two Sparse Matrices\n");
-		printf("4.Transpose of A Matrix\n");
+		printf("1.Accept a Sparse Matrix\n");
+		printf("2.Display a Sparse Matrix\n");
+		printf("3.Addition of the two Matrices\n");
+		printf("4.Transpose a Sparse Matrix\n");
+		printf("5.Exit\n");
 		scanf("%d",&opt);
 		switch(opt) {
 		case 1 : {
-			printf("1.Accept First Matrix\n2.Accept Second Matrix\n");
+			printf("1.Accept the First Matrix\n2.Accept the Second Matrix\n");
 			scanf("%d",&opt);
 			switch(opt) {
 			case 1 : {
-				printf("Enter the Number of Rows of Sparse Matrix\n");
-				scanf("%d",&row);
-				printf("Enter the Number of Columns of Sparse Matrix\n");
-				scanf("%d",&column);
-				a = mallocate(a,row,column);
-				accept(a,row,column);
-				convert(a,c,row,column);
+				printf("Enter the Number of Rows and Number of Columns\n");
+				scanf("%d%d",&row,&col);
+				accept(c,row,col);
 				break;
 			}
 			case 2 : {
-				printf("Enter the Number of Rows of Sparse Matrix\n");
-				scanf("%d",&row1);
-				printf("Enter the Number of Columns of Sparse Matrix\n");
-				scanf("%d",&column1);
-				b = mallocate(b,row1,column1);
-				accept(b,row1,column1);
-				convert(b,c1,row1,column1);
-				break;
+				printf("Enter the Number of Rows and Number of Columns\n");
+				scanf("%d%d",&row1,&col1);
+				accept(c1,row1,col1);
 			}
 			}
 			break;
@@ -96,19 +109,21 @@ int main() {
 			scanf("%d",&opt);
 			switch(opt) {
 			case 1 : {
-				display(c,row,column);
+				display(c);
 				break;
 			}
 			case 2 : {
-				display(c1,row1,column1);
+				display(c1);
 				break;
 			}
 			}
 			break;
 		}
 		case 3 : {
-
+			add(c,c1);
+			break;
 		}
 		}
 	}while(1);
 }
+
