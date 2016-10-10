be409f69ee8477b0730de9229257e753e51938c8
Ishan Joshi <ishanjoshi02@gmail.com>
2016-10-10 06:06:37 +0530

    Used Recursive Functions now

--- generalisedLinkedList.c
+++ generalisedLinkedList.c
@@ -1,90 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
-typedef struct {
+typedef struct node {
 	int flag;
 	char data;
 	struct node *next,*level;
 }node;
-node *space() {
-	return (node *)malloc(sizeof(node));
+node *getData(char string[],node *p) {
+	static int index = 0;
+	p = (node *)malloc(sizeof(node));
+	if(string[index++]=='(') {
+		p->flag = 1;
+		p->level = getData(string,p->level);
+	}
+	else if(string[index++]==')') {
+		return p;
+	}
+	else {
+		p->flag = 0;
+		p->data = string[index++];
+		p->next = getData(string,p->next);
+	}
 }
-node *create(node *head) {
-	head = space();
-	node *p = head,*q[5];
-	char c;
-	int temp = -1;
-	do {
-		scanf(" %c",&c);
-		if(c == '(') {
-			if(temp != -1) {
-				p->flag = 1;
-				q[temp] = p;
-				p->level = space();
-				p = p->level;
-			}
-			temp++;
-		}
-		else if(c == ')') {
-			temp-- ;
-			if(temp != -1) {
-				p = q[temp];
-				p->next = space();
-				p = p->next;
-			}
-		}
-		else {
-			p->flag = 0;
-			p->level = NULL;
-			p->data = c;
-			p->next = space();
-			p = p->next;
-			p->next = NULL;
-			p->level = NULL;
-		}
-	}while(temp != -1);
-	return head;
+void displayData(node *p) {
+	if(p==NULL) {
+		return;
+	}
+	if(p->flag == 0) {
+		printf("%c ",p->data);
+		displayData(p->next);
+		printf(")");
+	}
+	else {
+		printf("( ");
+		displayData(p->level);
+	}
 }
-void display(node *head) {
-	node *p = head,*q[5];
-	int temp = 0;
-	printf("(\t");
-	do {
-		if(p->flag == 0) {
-			printf("%c\t",p->data);
-			if(p->level == NULL && p->next == NULL && temp !=0) {
-				p = q[--temp];
-				p = p->next;
-				printf(")\t");
-			}
-		}
-		else {
-			q[temp++] = p;
-			p = p->level;
-			printf("(\t");
-		}
-	}while(temp != 0);
-	printf(")\n");
+int main(int argc, char **argv) {
+	char string[101];
+	node *head;
+	printf("Enter the Data now\n");
+	scanf("%s",string);
+	displayData(getData(string,head));
 }
-int main() {
-	node *head = NULL;
-	int temp;
-	do {
-		printf("1. Generate Generalized Linked List\n"
-				"2. Display the List\n"
-				"3. Exit\n");
-		scanf("%d",&temp);
-		switch(temp) {
-		case 1 : {
-			head = create(head);
-			break;
-		}
-		case 2 : {
-			display(head);
-			break;
-		}
-		case 3 : {
-			exit(0);
-		}
-		}
-	}while(1);
-}
+
