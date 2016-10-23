d701c0725e7bbd2bb735bcc9a95480d94f5dd4ae
Ishan Joshi <ishanjoshi02@gmail.com>
2016-10-24 05:21:51 +0530

    Update binarySearch.c

--- binarySearch.c
+++ binarySearch.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 void binarySearch(int a[],int key,int start,int end);
+void bubbleSort(int a[],int n);
 int main(int argc, char const *argv[]) {
   int key,elements;
   printf("Enter the Number of Elements\n");
@@ -10,10 +11,25 @@ int main(int argc, char const *argv[]) {
   for(int i=0;i<elements;i++) {
     scanf("%d",&a[i]);
   }
+  bubbleSort(a,elements);
   printf("Enter the Key Element\n");
   scanf("%d",&key);
   binarySearch(a,key,0,elements);
   return 0;
+}
+void bubbleSort(int a[],int n) {
+  int flag = 1,temp;
+  for(i=1;i<n && flag == 1;i++) {
+    flag = 0;
+    for(j=0;j<n-i;j++) {
+      if(a[j]>a[j+1]) {
+        flag = 1;
+        temp = a[j];
+        a[j] = a[j+1];
+        a[j+1] = temp;
+      }
+    }
+  }
 }
 void binarySearch(int a[],int key,int start,int end) {
   int mid = (start+end)/2;
