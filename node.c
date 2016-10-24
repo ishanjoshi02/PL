d2833761419619422f3457580da5b8339fe0d72c
Ishan Joshi <ishanjoshi02@gmail.com>
2016-10-24 06:20:38 +0530

    Update node.c

--- node.c
+++ node.c
@@ -1,208 +1,129 @@
-/*
-Implement singly linked list with following opt
-1 ins of a node at any location
-2 deletion of a node from any location
-3 display a list
-4 reverting a list without using additional data structure
- */
-
-
+//This is an extremely Crude Program. Little changes required\n. Please make those changes
+//Changes needed in addNode(),display().
 #include <stdio.h>
 #include <stdlib.h>
-
-typedef struct
-{
+typedef struct node {
 	int data;
 	struct node *next;
-
 }node;
-
-typedef struct {
-	int data;
-	struct n *previous;
-}n;
-
-node *create(int n);
-void display(node *head);
-node *insert(node *, int , int );
-node *del(node *, int);
-void displayrev(node *);
-void displayreverse(node *);
-
-int main()
-{
-	node *head;
-	int n, loc;
-	int opt, dt;
-	while(1)
-	{
-		printf("Choose your option\n1: Input data\n2: Display the linked list\n3: Insert a node at a location\n4:: Display reverse\n5: Delete node\n6: Exit\n");
-		scanf("%d", &opt);
-
-		switch(opt)
-		{
-		case 1:
-			printf("Enter no. of elements:\n");
-			scanf("%d", &n);
-			head = create(n);
-			break;
-
-		case 2:
-			display(head);
-			break;
-
-		case 3:
-			printf("In which position do you want to enter node?\n");
-			scanf("%d", &loc);
-			printf("Enter data for new node\n");
-			scanf("%d", &dt);
-			head = insert(head, loc, dt);
-			break;
-
-		case 5:
-			printf("Which node do you want to delete?\n");
-			scanf("%d", &loc);
-			head = del(head,loc);
-			break;
-
-		case 4:
-			displayrev(head);
-			break;
-
-		case 6:
-			exit(1);
-
-		}
-	}
-	return 0;
+node *addNode(node *p) {
+	p = (node *)malloc(sizeof(node));
+	printf("Enter Element\n");
+	scanf("%d",&p->data);
+	return p;
 }
-
-node *create(int n)
-{
-	int i=1, x;
-	node *head=NULL, *p;
-	head=(node*)malloc(sizeof(node));
-	printf("Enter data for node %d :\n", i);
-	scanf("%d", &x);
-	head->data=x;
-	head->next=NULL;
-	p=head;
-	for(i=2; i<=n; i++)
-	{
-		p->next=(node*)malloc(sizeof(node));
-		p=p->next;
-		printf("Enter data for node %d:\n", i);
-		scanf("%d", &x);
-		p->data=x;
-		p->next=NULL;
+node *create(node *head) {
+	node *p;
+	char another;
+	head = addNode(head);
+	printf("Do you want to add another node\n");
+	scanf(" %c",&another);
+	p = head->next;
+	while (another=='y'||another=='Y') {
+		p = addNode(p);
+		p = p->next;
+		printf("Do you want to add another node\n");
+		scanf(" %c",&another);
 	}
 	return head;
 }
-
-void display(node *head)
-{
-	int i;
-	node *p=head;
-	if(p==NULL)
-	{
-		printf("No data man! Enter data!\n");
-	}
-	else
-	{
-		while(p!=NULL)
-		{
-			printf("Data at node %d is :", ++i);
-			printf("%d\n", p->data);
-			p=p->next;
+void display(node *p,int n) {
+	if(p!=NULL) {
+		if(n==1) {
+			printf("%d\n",p->data);
+			display(p->next,n);
+		}
+		else {
+			display(p->next,n);
+			printf("%d\n",p->data);
 		}
 	}
 }
-
-node *insert(node *head, int loc, int dt)
-{
-	int i;
-	node *p, *q;
-	p=(node *)malloc(sizeof(node));
-	p->data=dt;
-	if(loc==1)
-	{
-		p->next=head;
-		head=p;
+node *addatLocation(node *head) {
+	int location;
+	node *p = head,*q=NULL;
+	printf("Enter Location\n");
+	scanf("%d",&location);
+	if(location == 0) {
+		q = addNode(q);
+		q->next = head;
+		return q;
 	}
-
-	else
-	{
-		q=head;
-		for(i=1; i<(loc-1); i++)
-		{
-			q=q->next;
-		}
-		if(q!=NULL)
-		{
-			p->next=q->next;
-			q->next=p;
-		}
-		else
-		{
-			printf("Overflow\n");
-		}
-	}
+	for(int i=0;i<location;i++,p=p->next){}
+	q = p->next;
+	p->next = addNode(p->next);
+	p = p->next;
+	p->next = q;
 	return head;
 }
-
-node *del(node *head, int loc)
-{
-	int i;
-	node *p, *q;
-	p=head;
-	if(loc==1)
-	{
-		head=head->next;
-		free(p);
+node *delete(node *head) {
+	int location;
+	node *p = head,*q;
+	printf("Enter Location\n");
+	scanf("%d",&location);
+	if(location!=0) {
+		for(int i=0;i<location;i++,p=p->next){}
+		q = p->next;
+		p->next = p->next->next;
 	}
-
-	else
-		q=head;
-	{
-		for(i=1; i<(loc-1); i++)
-		{
-			q=q->next;
-		}
-
-		if(q!=NULL)
-		{
-			p=q->next;
-			q->next=p->next;
-			free(p);
-		}
+	else {
+		q = head;
+		free(q);
+		return head->next;
 	}
+	free(q);
 	return head;
 }
-
-void  displayrev(node *head)
-{
-	if(head!=NULL)
-	{
-		displayrev(head->next);
-		printf("%d\n", head->data);
-	}
-}
-void displayreverse(node *head) {
-	n *p;
-	node *pf;
-	pf = head;
-	while(pf != NULL) {
-		pf = pf->next;
-	}
-	p = pf;
-	pf = head;
-	while(pf != NULL) {
-		p->data = pf->data;
-		p->previous = pf->next;
-		pf = pf->next;
-	}
+node *reverseList(node *head) {
+	node *p = head,*q = NULL,*r = NULL;
 	while(p!=NULL) {
-		printf("%d",p->data);
-		p = p->previous;
+		q = p->next;
+		p->next = r;
+		r = p;
+		p = q;
 	}
+	return r;
+}
+int main(int argc, char **argv) {
+	int option;
+	node *head;
+	do {
+		printf("1. Create List\n"
+				"2. Display List\n"
+				"3. Add a Node\n"
+				"4. Delete a Node\n"
+				"5. Display Reverse List\n"
+				"6. Reverse List\n"
+				"7. Exit \n");
+		scanf("%d",&option);
+		switch(option) {
+		case 1 : {
+			head = create(head);
+			break;
+		}
+		case 2 : {
+			display(head,1);
+			break;
+		}
+		case 3 : {
+			head = addatLocation(head);
+			break;
+		}
+		case 4 : {
+			head = delete(head);
+			break;
+		}
+		case 5 : {
+			display(head,12);
+			break;
+		}
+		case 6 : {
+			head = reverseList(head);
+			break;
+		}
+		case 7 : {
+			return 0;
+		}
+		}
+	} while (1);
 }
